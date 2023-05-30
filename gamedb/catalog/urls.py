12d9b5ebac1de2ec873adf746891eb17066e02ba
John Doe <johndoe@example.com>
2023-05-30 13:33:34 +0100

    Added edit/delete urls for reviews & comments

--- gamedb/catalog/urls.py
+++ gamedb/catalog/urls.py
@@ -11,3 +11,7 @@ urlpatterns = [
     path('', include("django.contrib.auth.urls")),
     path('submit_review/<int:pk>', views.submit_review, name='submit_review'),
     path('submit_comment/<int:pk>', views.submit_comment, name='submit_comment'),
+    path('accounts/profile/delete_review', views.delete_review, name='delete_review'),
+    path('accounts/profile/edit_review', views.edit_review, name='edit_review'),
+    path('accounts/profile/delete_reply', views.delete_reply, name='delete_reply'),
+    path('accounts/profile/edit_reply', views.edit_reply, name='edit_reply'),
