a613cd5f118f3d86ec62de764e15e2c7b1a63fe2
John Doe <johndoe@example.com>
2023-05-30 12:47:47 +0100

    Add registration path to urls

--- gamedb/catalog/urls.py
+++ gamedb/catalog/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('games/<int:pk>', views.game_description_view, name='game-description'),
     path('search_results/', views.search_results, name='search_results'),
+    path('register', views.register, name='register'),
