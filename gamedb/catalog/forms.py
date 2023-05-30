2e3106565fda277be51d3703eb6f40c56fdfe6c1
John Doe <johndoe@example.com>
2023-05-30 13:15:00 +0100

    Added Review & Comment forms to forms.py

--- gamedb/catalog/forms.py
+++ gamedb/catalog/forms.py
@@ -10,3 +10,31 @@ from crispy_forms.layout import Layout, Fieldset, Field, Div
 from crispy_forms.bootstrap import InlineRadios
 from star_ratings.models import Rating
 from catalog.models import Game_List
+
+
+class ReviewBox(forms.Form):
+    title = forms.CharField(max_length=200,required=True)
+    ratings = [('','')]
+    test_rating = Rating()
+    Review=forms.IntegerField(label='Review', widget=forms.RadioSelect(choices=ratings),required=True)
+    comment = forms.CharField(widget=TinyMCE(attrs={'cols': 125, 'rows': 5, "forced_root_block": 'p',"newline_behavior": 'block'}),required=True)
+    class Meta:
+        model = MyModel
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+                Div(
+            'title',
+            'comment'
+                ))
+
+class CommentBox(forms.Form):
+    title = forms.CharField(max_length=200)
+    comment = forms.CharField(widget=TinyMCE(attrs={'cols': 120, 'rows': 5,'theme':'advanced','force_p_newlines' : False, 'forced_root_block': False,'newline_behavior': 'block'}))
+    class Meta:
+        model = MyModel
+        fields = '__all__'
