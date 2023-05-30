c31a51f216d405d627192211edc4ebec90612dc9
John Doe <johndoe@example.com>
2023-05-30 12:40:22 +0100

    Add search function as basis for db population

--- gamedb/catalog/views.py
+++ gamedb/catalog/views.py
@@ -59,3 +59,49 @@ header = {
     "Client-ID": client_key,
     "Authorization": "Bearer " + access_token,
 }
+
+
+
+
+
+def search_results(request):
+    # Get the value of the 'search_term' parameter from the request's GET data
+    query = request.GET.get("search_term")
+
+    # Send a request to an API to search for games using the query term
+    y = requests.post(
+        "https://api.igdb.com/v4/games?fields=*,cover.*,involved_companies.*&search="
+        + query
+        + "&limit=30",
+        headers=header,
+    )
+
+    # Convert the response to JSON format
+    testimage = y.json()
+
+    # Create an empty list for storing search results
+    search_results = []
+
+    # Iterate over each item in testimage
+    for each in testimage:
+        # Append each item to the search_results list
+        search_results.append(each)
+
+        try:
+            # Try to retrieve a Game object with the corresponding id from the database
+            Game.objects.get(id=each["id"])
+        except Game.DoesNotExist:
+            # If the Game object does not exist, call the game_add function to add it
+            game_add(each)
+
+    # Create a Paginator object with search_results list and a limit of 15 items per page
+    paginator = Paginator(search_results, 15)
+
+    # Get the page number from the request's GET data
+    page_number = request.GET.get("page")
+
+    # Get the corresponding page object from the Paginator
+    page_obj = paginator.get_page(page_number)
+    context = {"tests": search_results, "page_obj": page_obj}
+    # Render the 'catalog/search_results.html' template with the given context
+    return render(request, "catalog/search_results.html", context=context)
