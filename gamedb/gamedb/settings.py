e48309f0fe400e4197058f23adc9c2f4f9aafa23
John Doe <johndoe@example.com>
2023-05-30 13:23:59 +0100

    Updated settings to include tinymce app etc.

--- gamedb/gamedb/settings.py
+++ gamedb/gamedb/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tinymce',
+    'widget_tweaks',
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
